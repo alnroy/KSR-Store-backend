cb879cc8be103ae12474901e45cfad62001fd657
alnroy <alanroyff101@gmail.com>
2026-03-11 18:01:19 +0530

    committing - Frontend UI and Logic

--- shop/models.py
+++ shop/models.py
@@ -13,6 +13,7 @@ class Brand(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='categories/', blank=True, null=True)
 
     def __str__(self):
         return self.name
